e1298e6166e0c2c106efa9014081a20fe4b66d20
karinaaltheaabad <karinaaltheaabad@users.noreply.github.com>
2020-04-27 13:53:38 -0700

    Initial Commit

---
+++ ToDoMidtermP1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoMidtermP1
+//
+//  Created by Karina Abad on 4/27/20.
+//  Copyright © 2020 Karina Abad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
